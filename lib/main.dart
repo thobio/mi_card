1e0ebb13b957e1738991cc7f98e24b61238b4de2
thobio <thobio.j@zerone-consulting.com>
2020-02-19 19:28:18 +0530

    Full profile Card

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,68 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/profile.png'),
-              )
+                radius: 60.0,
+                backgroundImage: AssetImage('images/my.jpg'),
+              ),
+              Text(
+                'Thobio Joseph',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'iOS Developer | Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source',
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+91 9048 128 845",
+                    style: TextStyle(
+                        fontFamily: 'Source',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "jthobio2@gmail.com",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
